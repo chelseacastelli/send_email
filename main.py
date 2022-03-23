8034ba7e2aa9533d67f2c5b95b99375aad6441df
Chelsea Castelli <chel.castel@aol.com>
2022-03-23 15:17:01 -0400

    send email periodically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-import yagmail, os
+import yagmail, os, time
 
 sender = 'castelc595@gmail.com'
 receiver = 'xfrwfdysb@zeroe.ml'
@@ -10,7 +10,10 @@ contents = """
 Here is the content of the email
 """
 
-yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
-yag.send(to=receiver, subject=subject, contents=contents)
 
-print('Email Sent!')
+while True:
+    yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
+    yag.send(to=receiver, subject=subject, contents=contents)
+    print('Email Sent!')
+    time.sleep(60)
+
