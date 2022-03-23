987dc4ee239513ef7be97c78b68fbbb1b6395e4e
Chelsea Castelli <chel.castel@aol.com>
2022-03-23 16:37:00 -0400

    send email every day at certain time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import yagmail, os, time
+from datetime import datetime as dt
 
 sender = 'castelc595@gmail.com'
 receiver = 'xfrwfdysb@zeroe.ml'
@@ -10,10 +11,13 @@ contents = """
 Here is the content of the email
 """
 
-
 while True:
-    yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
-    yag.send(to=receiver, subject=subject, contents=contents)
-    print('Email Sent!')
-    time.sleep(60)
-
+    now = dt.now()
+    # Send email everyday @ 1:15pm
+    if now.hour == 13 and now.minute == 15:
+        yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
+        yag.send(to=receiver, subject=subject, contents=contents)
+        print('Email Sent!')
+        # check the time every 60s (1 minute) to see if it's 1:15
+        time.sleep(60)
+    
